2cbed9e9c4f6b2e347760cf6c30940b6a80cf7d6
NataliaLKB <natalialeebaltazar@gmail.com>
2014-10-13 23:01:16 +0100

    no jquery!!

--- script.js
+++ script.js
@@ -1,88 +1,79 @@
-$(document).ready(function() {
+var rtime = function (dateString) {
+  var rightNow = new Date();
+  var then = new Date(dateString);
 
-  var jsonstring = "";
-  var htmlToWrite ="";
-  var toWrite = "";
+  var diff = rightNow - then;
+  var second = 1000;
+  var minute = second * 60;
+  var hour = minute * 60;
+  var day = hour * 24;
 
-  var pulldata = function() {
-
-    $.ajax({
-        type: "GET",
-        cache: false,
-        dataType: "JSON",
-        url: "http://neapolitan.herokuapp.com/",
-        success: function(data) {
-          jsonstring = data;
-          massageData(jsonstring);
-        },
-        error: function (errorT) {
-          alert(errorT);
-        }
-    });
-  };
-
-  function massageData(twstring) {
-    twstring.statuses.forEach(function(statuses) {
-      if (typeof statuses.entities.media !== 'undefined') {
-        toWrite += "<div class='col-md-6 col-sm-6 col-xs-12'>" + 
-          "<div class='thumbnail'>" + "<div id='outer'>" + 
-          "<img src='" + statuses.entities.media[0].media_url + "'></div>" +
-          "<div class='caption'>" +
-          "<p>" + rtime(statuses.created_at) + "</p>" +
-          "<h4>" + statuses.user.name + "</h4>" +
-          "</div></div></div>";
-      }
-    });
-
-
-    $("#content").append(
-      toWrite
-    );
+  if (isNaN(diff) || diff < 0) {
+    return "";
   }
+  if (diff < second * 2) {
+    return "right now";
+  }
+  if (diff < minute) {
+    return Math.floor(diff / second) + " seconds ago";
+  }
+  if (diff < minute * 2) {
+    return "about 1 minute ago";
+  }
+  if (diff < hour) {
+    return Math.floor(diff / minute) + " minutes ago";
+  }
+  if (diff < hour * 2) {
+    return "about 1 hour ago";
+  }
+  if (diff < day) {
+    return Math.floor(diff / hour) + " hours ago";
+  }
+  if (diff > day && diff < day * 2) {
+    return "yesterday";
+  }
+  if (diff < day * 365) {
+    return Math.floor(diff / day) + " days ago";
+  } else {
+    return "over a year ago";
+  }
+};
 
-  pulldata();
+var toWrite = "";
+var httpRequest = new XMLHttpRequest();
 
-  rtime = function(dateString) {
-    var rightNow = new Date();
-    var then = new Date(dateString);
+function fetchJSONFile(path, callback) {
 
-    var diff = rightNow - then;
-    var second = 1000;
-    var minute = second * 60;
-    var hour = minute * 60;
-    var day = hour * 24;
-    var week = day * 7;
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
+      }
+    }
+  };
+}
 
-    if (isNaN(diff) || diff < 0) {
-        return "";
-    }
-    if (diff < second * 2) {
-        return "right now";
-    }
-    if (diff < minute) {
-        return Math.floor(diff / second) + " seconds ago";
-    }
-    if (diff < minute * 2) {
-        return "about 1 minute ago";
-    }
-    if (diff < hour) {
-        return Math.floor(diff / minute) + " minutes ago";
-    }
-    if (diff < hour * 2) {
-        return "about 1 hour ago";
-    }
-    if (diff < day) {
-        return  Math.floor(diff / hour) + " hours ago";
-    }
-    if (diff > day && diff < day * 2) {
-        return "yesterday";
-    }
-    if (diff < day * 365) {
-        return Math.floor(diff / day) + " days ago";
-    }
-    else {
-        return "over a year ago";
-    }
-  }; // timeAgo()
+httpRequest.open('GET', "http://neapolitan.herokuapp.com/");
+httpRequest.send(); 
 
+  // this requests the file and executes a callback with the parsed result once
+  //   it is available
+fetchJSONFile("http://neapolitan.herokuapp.com/", function(data){
+  data.statuses.forEach(function (statuses) {
+    if (typeof statuses.entities.media !== 'undefined') {
+      toWrite += "<div class='col-md-6 col-sm-6 col-xs-12'>" +
+        "<div class='thumbnail'>" + "<div id='outer'>" +
+        "<img src='" + statuses.entities.media[0].media_url + "'></div>" +
+        "<div class='caption'>" +
+        "<p>" + rtime(statuses.created_at) + "</p>" +
+        "<h4>" + statuses.user.name + "</h4>" +
+        "</div></div></div>";
+    }
+  console.log(toWrite);
+  var parser = new DOMParser()
+  var html = parser.parseFromString(toWrite, "text/html");
+  var status = html.childNodes[0];
+  document.getElementById("content").appendChild(status);
+  });
 });
